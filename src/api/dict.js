45084baa1a5e42d419583112c0e7718f2403959c
Username <Username@example.com>
2022-01-24 23:47:27 +0800

    第四次提交59：医院通-后台系统-数据字典前端-列表

--- src/api/dict.js
+++ src/api/dict.js
@@ -4,6 +4,7 @@ export default {
   //数据字典列表
   dictList(id) {
     return request ({
+      // 这个请求的路径要跟后端的cmn控制器中的路径保持一致
       url: `/admin/cmn/dict/findChildData/${id}`,
       method: 'get'
     })
